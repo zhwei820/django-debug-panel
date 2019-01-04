efd759c39897860a34f6c596f2b9bf7f2544f8a2
zw <zw@knownsec.com>
2019-01-04 18:31:41 +0800

    fix: django 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-debug-panel',
-    version='0.8.3',
+    version='0.8.4',
     author=u'Joël Billaud',
     author_email='jbillaud@gmail.com',
     packages=['debug_panel'],
